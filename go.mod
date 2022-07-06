2ed286b48d9ea71c9850167bc82a467cb60858ba
agam.p <agamp94@gmail.com>
2022-07-06 20:54:25 +0700

    try upgrade major on gomodules

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/agamp94/app-say-hello
 
 go 1.18
+
+require github.com/agamp94/go-say-hello/v2 v2.0.0 // indirect
--- main.go
+++ main.go
@@ -1,5 +1,11 @@
 package main
 
+import (
+	"fmt"
+	go_say_hello "github.com/agamp94/go-say-hello/v2"
+)
+
 func main() {
 
+	fmt.Println(go_say_hello.SayHello("Agam"))
 }
